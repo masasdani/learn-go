73e35c606fd1796554b9f603e3b76ff9dc9ab7c4
masas <masasdn@gmail.com>
2024-03-18 22:28:32 +0700

    restrucute fiber skeleton, add expernal config support

--- http-fiber/main.go
+++ http-fiber/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
-	"masasdani.com/http-fiber/config"
-	"masasdani.com/http-fiber/controller"
+	"masasdani.com/http-fiber/internal/controller"
+	"masasdani.com/http-fiber/pkg"
 )
 
 func main() {
 	// ...
 	log.SetLevel(log.LevelInfo)
+
+	pkg.InitConfig()
+
 	app := fiber.New(fiber.Config{
 		// Override default error handler
-		ErrorHandler: config.ErrorHandlers,
+		ErrorHandler: pkg.ErrorHandlers,
 	})
 
-	applyController(app)
+	controller.ApplyController(app)
 
-	app.Listen(":8000")
-}
-
-func applyController(app *fiber.App) {
-	app.Get("/", controller.DefaultController)
+	app.Listen(fmt.Sprintf(":%v", pkg.AppConfig.Server.Port))
 }
